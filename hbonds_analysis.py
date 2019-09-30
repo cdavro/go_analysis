3d2ef0a2d904b61b4e4efe68e5a10f695a9e6573
Rolf David <rdavid@lsu.edu>
2019-09-30 16:04:28 -0500

    expand and reformat

--- hbonds_analysis.py
+++ hbonds_analysis.py
@@ -1,43 +1,51 @@
+# coding: utf-8
+import sys
+import os.path
 import numpy as np
 
-hbonds_revPBED3={}
+if ((sys.argv[1]) == ''):
+    ext='txt'
+else:
+    ext=(sys.argv[1])
 
-list_filename=["01","02","03","04","05"]
+list_filename=["00","01","02","03","04","05"]
 
 list_type=["all","0","1N","1S","2S","3S","2D","3D","4D","5D"]
 list_donnor=[-1,0,0,1,1,1,2,2,2,2]
 list_acceptor=[-1,0,1,0,1,2,0,1,2,3]
 
-
 list_L=["all","L1","L2","L3"]
-list_Ld=[-20.0,-20.0,2.75,5.25]
-list_Lu=[20.0,2.75,5.25,20.0]
+#list_Ld=[-20.0,-20.0,2.75,5.25]
+#list_Lu=[20.0,2.75,5.25,20.0]
+list_Ld=[-20.0,-20.0,3.25,6.50]
+list_Lu=[20.0,3.25,6.50,20.0]
 
-for i in list_filename:
-    with open (i+'_O_hbonds.dat') as f:
-        hbonds_revPBED3[i] = np.genfromtxt(f,dtype='float64',skip_header=1)
+hbonds=np.zeros((1,6))
+for f in list_filename:
+    filename = (f+'_O_hbonds.'+ext)
+    if (os.path.isfile(filename)):
+        hbonds = np.append(hbonds, np.genfromtxt(filename,dtype='float64',skip_header=1,usecols=(1,3,4,9,10,11)), axis=0)
+hbonds=np.delete(hbonds,(0),axis=0)
 
 oh={}
 for n,o,p in zip(list_type,list_donnor,list_acceptor):
     oh[n]={}
     for k,l,m in zip(list_L,list_Ld,list_Lu):
-
         oh[n][k]=0
-        for i in list_filename:
-            if n == 'all':
-                oh[n][k] = oh[n][k] + np.count_nonzero(hbonds_revPBED3[i][(hbonds_revPBED3[i][:,10]==13)
-                    & (hbonds_revPBED3[i][:,9]==1)
-                    & (hbonds_revPBED3[i][:,11]>l)
-                    & (hbonds_revPBED3[i][:,11]<=m)
-                    ][:,1])
-            else:
-                oh[n][k] = oh[n][k] + np.count_nonzero(hbonds_revPBED3[i][(hbonds_revPBED3[i][:,10]==13)
-                    & (hbonds_revPBED3[i][:,9]==1)
-                    & (hbonds_revPBED3[i][:,11]>l)
-                    & (hbonds_revPBED3[i][:,11]<=m)
-                    & (hbonds_revPBED3[i][:,3]==o)
-                    & (hbonds_revPBED3[i][:,4]==p)
-                    ][:,1])
+        if n == 'all':
+            oh[n][k] = oh[n][k] + np.count_nonzero(hbonds[(hbonds[:,4]==13)
+                & (hbonds[:,3]==1)
+                & (hbonds[:,5]>l)
+                & (hbonds[:,5]<=m)
+                ][:,0])
+        else:
+            oh[n][k] = oh[n][k] + np.count_nonzero(hbonds[(hbonds[:,4]==13)
+                & (hbonds[:,3]==1)
+                & (hbonds[:,5]>l)
+                & (hbonds[:,5]<=m)
+                & (hbonds[:,1]==o)
+                & (hbonds[:,2]==p)
+                ][:,0])
 print(oh)
 for n,o,p in zip(list_type,list_donnor,list_acceptor):
     i = 0
@@ -55,21 +63,20 @@ for n,o,p in zip(list_L,list_Ld,list_Lu):
     oh[n]={}
     for k,l,m in zip(list_type,list_donnor,list_acceptor):
         oh[n][k]=0
-        for i in list_filename:
-            if k == 'all':
-                oh[n][k] = oh[n][k] + np.count_nonzero(hbonds_revPBED3[i][(hbonds_revPBED3[i][:,10]==13)
-                    & (hbonds_revPBED3[i][:,9]==1)
-                    & (hbonds_revPBED3[i][:,11]>o)
-                    & (hbonds_revPBED3[i][:,11]<=p)
-                    ][:,1])
-            else:
-                oh[n][k] = oh[n][k] + np.count_nonzero(hbonds_revPBED3[i][(hbonds_revPBED3[i][:,10]==13)
-                    & (hbonds_revPBED3[i][:,9]==1)
-                    & (hbonds_revPBED3[i][:,11]>o)
-                    & (hbonds_revPBED3[i][:,11]<=p)
-                    & (hbonds_revPBED3[i][:,3]==l)
-                    & (hbonds_revPBED3[i][:,4]==m)
-                    ][:,1])
+        if k == 'all':
+            oh[n][k] = oh[n][k] + np.count_nonzero(hbonds[(hbonds[:,4]==13)
+                & (hbonds[:,3]==1)
+                & (hbonds[:,5]>o)
+                & (hbonds[:,5]<=p)
+                ][:,0])
+        else:
+            oh[n][k] = oh[n][k] + np.count_nonzero(hbonds[(hbonds[:,4]==13)
+                & (hbonds[:,3]==1)
+                & (hbonds[:,5]>o)
+                & (hbonds[:,5]<=p)
+                & (hbonds[:,1]==l)
+                & (hbonds[:,2]==m)
+                ][:,0])
 print(oh)
 
 for n,o,p in zip(list_L,list_Ld,list_Lu):
