69eba0eeeb97ce0ab208f6593245d8f825addbf2
Rolf David <rdavid@lsu.edu>
2020-04-28 18:17:09 -0500

    Added more fluctuation criteria

--- fluctuation.f90
+++ fluctuation.f90
@@ -21,10 +21,12 @@ INTEGER                         :: CAC
 !   ----------------------------------------------- Infos/properties
 REAL(dp), ALLOCATABLE           :: atm_mat(:,:,:)
 CHARACTER(LEN=2), ALLOCATABLE   :: atm_el(:)
-REAL(dp), ALLOCATABLE           :: avg_z(:)
+REAL(dp)                        :: avg_z, CO_disp_vec(3),CO_disp_norm
 
 !   ----------------------------------------------- Counters
-INTEGER                         :: i, s, o
+INTEGER                         :: i, s, o ,j, k
+INTEGER, ALLOCATABLE            :: error_c(:)
+INTEGER                         :: error_t
 
 !   -----------------------------------------------
 PRINT'(A100)','--------------------------------------------------'&
@@ -56,7 +58,7 @@ PRINT'(A100)', '--------------------------------------------------'&
 , '--------------------------------------------------'
 
 !   ----------------------------------------------- Allocate function for reading files
-ALLOCATE(atm_mat(8,nb_atm,nb_step))
+ALLOCATE(atm_mat(9,nb_atm,nb_step))
 atm_mat(:,:,:) = 0.0_dp
 
 ! A ----------------------------------------------- Read positions
@@ -114,47 +116,86 @@ PRINT'(A40,F14.2,A20)', "Positions:", finish-start, "seconds elapsed"
 
 ! D -----------------------------------------------
 start = OMP_get_wtime()
-
-! AVGz of sheet
-ALLOCATE(avg_z(nb_step))
-
-avg_z(:) = 0.0_dp
+ALLOCATE(error_c(nb_step))
 
 !$OMP PARALLEL DO DEFAULT(NONE) SHARED(atm_mat, box, nb_step, nb_atm)&
-!$OMP SHARED(fluct_center_atmnb)&
-!$OMP SHARED(avg_z)&
-!$OMP PRIVATE(s, i, o)
+!$OMP SHARED(fluct_center_atmnb, error_c)&
+!$OMP PRIVATE(CO_disp_norm, CO_disp_vec, avg_z)&
+!$OMP PRIVATE(s, i, o, j)
 DO s = 1, nb_step
     o = 0
+    avg_z = 0.0_dp
     DO i = 1, nb_atm
         IF (atm_mat(2,i,s) .EQ. fluct_center_atmnb) THEN
-            avg_z(s) = avg_z(s) + atm_mat(6,i,s)
+            avg_z = avg_z + atm_mat(6,i,s)
             o = o + 1
         END IF
     END DO
-    avg_z(s) = avg_z(s) / o
+    avg_z = avg_z / o
     D1:DO i = 1, nb_atm
-        atm_mat(7,i,s) = avg_z(s)
+        atm_mat(7,i,s) = avg_z
         IF (atm_mat(2,i,s) .EQ. fluct_center_atmnb) THEN
-            atm_mat(8,i,s) = atm_mat(6,i,s) - avg_z(s)
+            atm_mat(8,i,s) = atm_mat(6,i,s) - avg_z
+         D2:DO j = 1, nb_atm
+            IF (atm_mat(2,j,s) .EQ. 10) THEN
+                DO k=1,3
+                    CO_disp_vec(k) = atm_mat(k+3,j,s) - atm_mat(k+3,i,s)
+                    CO_disp_vec(k) = CO_disp_vec(k) - box(k) * ANINT(CO_disp_vec(k)/box(k))
+                END DO
+                CO_disp_norm = NORM2(CO_disp_vec)
+                IF (CO_disp_norm .LT. fluct_OpC_rcut) THEN
+                    IF (atm_mat(9,i,s) .NE. 0.0) THEN
+                        error_c = error_c + 1
+                    END IF
+                    atm_mat(9,i,s) = 10
+                END IF
+            ELSE IF (atm_mat(2,j,s) .EQ. 11) THEN
+                DO k=1,3
+                    CO_disp_vec(k) = atm_mat(k+3,j,s) - atm_mat(k+3,i,s)
+                    CO_disp_vec(k) = CO_disp_vec(k) - box(k) * ANINT(CO_disp_vec(k)/box(k))
+                END DO
+                CO_disp_norm = NORM2(CO_disp_vec)
+                IF (CO_disp_norm .LT. fluct_OpC_rcut) THEN
+                    IF (atm_mat(9,i,s) .NE. 0.0) THEN
+                        error_c = error_c + 1
+                    END IF
+                    atm_mat(9,i,s) = 11
+                END IF
+            ELSE IF (atm_mat(2,j,s) .EQ. 12) THEN
+                DO k=1,3
+                    CO_disp_vec(k) = atm_mat(k+3,j,s) - atm_mat(k+3,i,s)
+                    CO_disp_vec(k) = CO_disp_vec(k) - box(k) * ANINT(CO_disp_vec(k)/box(k))
+                END DO
+                CO_disp_norm = NORM2(CO_disp_vec)
+                IF (CO_disp_norm .LT. fluct_OpC_rcut) THEN
+                    IF (atm_mat(9,i,s) .NE. 0.0) THEN
+                        error_c = error_c + 1
+                    END IF
+                    atm_mat(9,i,s) = 12
+                END IF
+            END IF
+            END DO D2
         END IF
     END DO D1
 END DO
 !$OMP END PARALLEL DO
 
+error_t = SUM(error_c(:))
+
 OPEN(UNIT=41, FILE = suffix//"_fluct_avgz_c.txt")
-WRITE(41, '(A10,A10,A24,A24)') "Step", "id", "avg_z", "z_fluct"
+WRITE(41, '(A10,A10,A24,A24,A10)') "Step", "id", "avg_z", "z_fluct", "type"
 DO s = 1, nb_step
     DO i = 1, nb_atm
         IF (atm_mat(2,i,s) .EQ. fluct_center_atmnb) THEN
-            WRITE(41, '(I10,I10,E24.14,E24.14)') s, INT(atm_mat(1,i,s))&
-            , atm_mat(7,i,s), atm_mat(8,i,s)
+            WRITE(41, '(I10,I10,E20.7,E20.7,I10)') s, INT(atm_mat(1,i,s))&
+            , atm_mat(7,i,s), atm_mat(8,i,s), INT(atm_mat(9,i,s))
         END IF
     END DO
 END DO
 CLOSE(UNIT=41)
 
 finish = OMP_get_wtime()
+PRINT'(A40,I10)', "Errors:", error_t
 PRINT'(A40,F14.2,A20)', "Fluctuation profiles:", finish-start, "seconds elapsed"
 
 !   ----------------------------------------------- End
@@ -163,6 +204,6 @@ PRINT'(A100)', '--------------------------------------------------'&
 PRINT'(A100)', 'The END'
 
 !   ----------------------------------------------- Deallocate and exit
-DEALLOCATE(atm_mat,avg_z)
+DEALLOCATE(atm_mat)
 
 END PROGRAM fluctuation
